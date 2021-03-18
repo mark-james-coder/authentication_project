a66b91e4daeba6db34772dcac816dc62767414cb
Mark <markjpoole@yahoo.com>
2021-03-18 12:37:10 +0000

    use env variable to hide secret, change secret and add .env to gitignore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -5,6 +6,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -17,8 +20,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
