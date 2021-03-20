67d09ded1b63a8b2dadd72c1500f5833588b0915
Mark <markjpoole@yahoo.com>
2021-03-20 16:05:26 +0000

    add buttons for signing up/logging in through Google

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
